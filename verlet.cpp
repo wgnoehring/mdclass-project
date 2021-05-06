86711cab90f1e15175acb5b705f70b6e65b41424
wnoehring <wolfram.noehring@imtek.uni-freiburg.de>
2021-05-06 15:29:20 +0200

    Added comment

--- verlet.cpp
+++ verlet.cpp
@@ -1,16 +1,22 @@
 //
 // Created by wnoehring on 4/27/21.
 //
+//
+/* Verlet integration */
 
 #include "types.h"
 
-void verlet_step1(Positions_t &positions, Velocities_t &velocities, const Forces_t &forces, double timestep, double mass) {
-    double dtm = 0.5 * timestep / mass;
-    velocities += forces * dtm;
-    positions += velocities * timestep;
+void verlet_step1(Positions_t &positions, Velocities_t &velocities, const Forces_t &forces, double timestep, Masses_t masses) {
+    for (int i = 0; i < velocities.rows(); ++i) {
+        velocities.row(i) += forces.row(i) * 0.5 * timestep / masses.transpose();
+    }
+    for (int i = 0; i < positions.rows(); ++i) {
+        positions.row(i) += velocities.row(i) * timestep;
+    }
 }
 
-void verlet_step2(Velocities_t &velocities, const Forces_t &forces, double timestep, double mass) {
-    double dtm = 0.5 * timestep / mass;
-    velocities += forces * dtm;
+void verlet_step2(Velocities_t &velocities, const Forces_t &forces, double timestep, Masses_t masses) {
+    for (int i = 0; i < velocities.rows(); ++i) {
+        velocities.row(i) += forces.row(i) * 0.5 * timestep / masses.transpose();
+    }
 }
